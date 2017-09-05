2cc2504b9a3cab73a49009655d1f294f11fabd5e
Abhishek Das <johnsonadam98@gmail.com>
2017-09-05 13:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     var phrases = [
+        'I may not have always shown it, but Im gratefull to have had such an awesome teacher like you.',
         'Thank You',
         'For Being A WonderFull Teacher.',
         'Happy Teachers Day',
